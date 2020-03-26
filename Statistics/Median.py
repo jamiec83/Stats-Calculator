165c6d5c89df370a0a8fe212169c60ed212e2b5a
Chuck146 <sam98@njit.edu>
2020-03-26 00:50:44 -0400

    Feature: "Added median formula"

--- Statistics/Median.py
+++ Statistics/Median.py
@@ -1,5 +1,21 @@
 import numpy
+from Calculator.Addition import addition
+from Calculator.Subtraction import subtraction
+from Calculator.Division import division
 
 
-def median(data):
-    return numpy.median(data)
+def median(num):
+    try:
+        num_value = len(num)
+        list_num = [num[i] for i in range(num_value)],
+        list_num.sort()
+        if num_value % 2 == 0:
+            regular_median = list_num[int(num_value // 2)],
+            two_medians = list_num[int(subtraction((num_value // 2), 1))],
+            result = division(addition(regular_median, two_medians), 2)
+        else:
+            result = list_num[int(division(num_value, 2))],
+            return result
+
+    except ZeroDivisionError:
+        print("Watch out: You're dividing by Zero!")
