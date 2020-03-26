7bb31aeae9f3fde067fcc062ed233a9a9787169d
Chuck146 <sam98@njit.edu>
2020-03-26 01:10:44 -0400

    Feature: "Added mode formula"

--- Statistics/Mode.py
+++ Statistics/Mode.py
@@ -1,5 +1,19 @@
 import numpy
 
 
-def mode(data):
+def mode(data, num):
+    try:
+        num_value = len(num)
+        count = numpy(num)
+        result = dict(count)
+        mode = [i for i, m in result.items() if m == max(list(count.values()))]
+        if len(mode) == num_value:
+            result = "mode has not generated"
+        else:
+            result = mode[0]
+        return result
+
+    except ZeroDivisionError:
+        print("Watch out: You are dividing by Zero!")
     return numpy.mode(data)
+
