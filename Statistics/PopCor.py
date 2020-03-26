3f8212c3685f1e96073eb25945062b89cd67c190
Chuck146 <sam98@njit.edu>
2020-03-26 01:54:48 -0400

    Task: "Completed imports to stats module to create class"

---
+++ Statistics/PopCor.py
@@ -0,0 +1,11 @@
+from Statistics.ZScore import ZScore
+
+def pop_correlation(data, data_1):
+    try:
+        z1 = ZScore(data)
+        z2 = ZScore(data_1)
+        z1_and_z2 = list(map(lambda x, y: x * y, z1, z2))
+        correlate = sum(z1_and_z2) / len(z1_and_z2)
+        return round(correlate, 7)
+    except ZeroDivisionError:
+        print("Watch out: You are dividing by Zero!")
--- Statistics/statistics.py
+++ Statistics/statistics.py
@@ -1,4 +1,16 @@
 from Calculator.Calculator import Calculator
+from Statistics.Mean import mean
+from Statistics.MeanDev import mean_dev
+from Statistics.Median import median
+from Statistics.Mode import mode
+from Statistics.PopCor import pop_correlation
+from Statistics.Quartiles import quarts
+from Statistics.SampleCor import sample_cor
+from Statistics.Std_Deviation import sdeviation
+from Statistics.Variance import variance
+from Statistics.ZScore import ZScore
+from CsvReader.CsvReader import CsvReader
+
 
 
 class Statistics(Calculator):
@@ -6,3 +18,4 @@ class Statistics(Calculator):
 
     def __init__(self):
         super().__init__()
+        
