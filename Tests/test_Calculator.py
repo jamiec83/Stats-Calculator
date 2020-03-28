f8bb30c8cd6a75a00ce2babc8a59eaec7ca239d3
jamiec83 <Jc682000!>
2020-03-28 11:44:42 -0400

    Updated: Unit Testing Output

--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -2,7 +2,7 @@ import unittest
 
 from Calculator.Calculator import Calculator
 from CsvReader.CsvReader import CsvReader
-
+from pprint import pprint
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
@@ -15,6 +15,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.calculator.add(1, 1), 2)
         self.assertEqual(self.calculator.result, 2)
         test_data = CsvReader('Tests/Data/Addition.csv').data
+        pprint("Addition:OK")
         for row in test_data:
             result = float(row['Result'])
             self.assertEqual(self.calculator.add(row['Value 2'], row['Value 1']), result)
@@ -24,6 +25,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.calculator.subtract(4, 4), 0)
         self.assertEqual(self.calculator.result, 0)
         test_data = CsvReader('Tests/Data/Subtraction.csv').data
+        pprint("Subtraction:OK")
         for row in test_data:
             result = float(row['Result'])
             self.assertEqual(self.calculator.subtract(row['Value 2'], row['Value 1']), result)
@@ -33,6 +35,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.calculator.multiply(3, 2), 6)
         self.assertEqual(self.calculator.result, 6)
         test_data = CsvReader('Tests/Data/Multiplication.csv').data
+        pprint("Multiplication:OK")
         for row in test_data:
             result = float(row['Result'])
             self.assertEqual(self.calculator.multiply(row['Value 2'], row['Value 1']), result)
@@ -42,6 +45,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.calculator.divide(5, 5), 1)
         self.assertEqual(self.calculator.result, 1)
         test_data = CsvReader('Tests/Data/Division.csv').data
+        pprint("Division:OK")
         for row in test_data:
             result = float(row['Result'])
             self.assertEqual(self.calculator.divide(row['Value 2'], row['Value 1']), result)
@@ -51,6 +55,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.calculator.square(2), 4)
         self.assertEqual(self.calculator.result, 4)
         test_data = CsvReader('Tests/Data/Square.csv').data
+        pprint("Square:OK")
         for row in test_data:
             result = float(row['Result'])
             self.assertEqual(self.calculator.square(row['Value 1']), result)
@@ -60,6 +65,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.calculator.squareroot(4), 2)
         self.assertEqual(self.calculator.result, 2)
         test_data = CsvReader('Tests/Data/SquareRoot.csv').data
+        pprint("Square Root:OK")
         for row in test_data:
             result = float(row['Result'])
             self.assertEqual(self.calculator.squareroot(row['Value 1']), result)
--- Tests/test_Population.py
+++ Tests/test_Population.py
@@ -5,30 +5,36 @@ from PopulationSampling.knownpopulation import ssk
 from PopulationSampling.marginoferror import marginerror
 from PopulationSampling.unknownpopulationsd import ssu
 from Statistics.statistics import Statistics
+from pprint import pprint
 
 class MyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
         self.statistics = Statistics()
 
-
     def test_results_property_calculator(self):
         self.assertEqual(self.statistics.result, 0)
 
     def test_cochran_statistics(self):
-            print(Cochrans(set))
+        pprint("Cochrans:Ok")
+        print(Cochrans(set))
 
     def test_conf_statistics(self):
-            print(ConfInt(set))
+        pprint("Confidence Interval:Ok")
+        print(ConfInt(set))
 
     def test_kpop_statistics(self):
-            print(ssk(set))
+        pprint("Known Population:Ok")
+        print(ssk(set))
 
     def test_margin_statistics(self):
-            print(marginerror(set))
+        pprint("Margin Of Error:Ok")
+        print(marginerror(set))
 
     def test_upop_statistics(self):
-            print(ssu(set))
+        pprint("Unknown Population:Ok")
+        print(ssu(set))
+
 
 if __name__ == '__main__':
     unittest.main()
