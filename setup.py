b916268827ada86bbea11ba573c28174baa165d7
jamiec83 <Jc682000!>
2020-03-28 21:07:31 -0400

    Added: Setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='Stats Calculator',
+      version='1.0.0',
+      description='This is a calculator that performs statistics, population functions and random num. gen.functions.',
+      authors='Rahul Araveti, Steven Morales, Jamie Cobin',
+      url='https://github.com/jamiec83/Stats-Calculator',
+      packages=find_packages(),
+     )
